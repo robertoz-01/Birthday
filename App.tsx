2f44de4b59783722913f04fbfb6d28f882dc9e66
Roberto Zanon <roberto.zanon.01@gmail.com>
2020-06-27 10:48:02 +0200

    Add react navigation tabs

--- App.tsx
+++ App.tsx
@@ -1,31 +1,25 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {SafeAreaView, StyleSheet, ScrollView, StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import BirthdayForm from './src/BirthdayForm';
 import BirthdayList from './src/BirthdayList';
 import BirthdayProvider from './src/contexts/BirthdayProvider';
 
+const Tab = createBottomTabNavigator();
+
 const App = () => {
   return (
-    <BirthdayProvider>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <BirthdayForm />
-          <BirthdayList />
-        </ScrollView>
-      </SafeAreaView>
-    </BirthdayProvider>
+    <NavigationContainer>
+      <BirthdayProvider>
+        <Tab.Navigator>
+          <Tab.Screen name="Birthdays" component={BirthdayList} />
+          <Tab.Screen name="Add Birthday" component={BirthdayForm} />
+        </Tab.Navigator>
+      </BirthdayProvider>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-});
 
 export default App;
