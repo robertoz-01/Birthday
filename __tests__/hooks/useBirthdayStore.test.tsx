af6a6e74ef580e43148bedc3e67a019c2f09498a
Roberto Zanon <roberto.zanon.01@gmail.com>
2020-06-28 21:59:50 +0200

    Add nextBirthday function in the cursom hook

--- __tests__/hooks/useBirthdayStore.test.tsx
+++ __tests__/hooks/useBirthdayStore.test.tsx
@@ -1,21 +1,54 @@
 import React from 'react';
 import {renderHook, act} from '@testing-library/react-hooks';
-import useCounter from '../../src/hooks/useBirthdayStore';
+import {addMonths, subYears} from 'date-fns';
+import useBirthdayStore from '../../src/hooks/useBirthdayStore';
 import BirthdayProvider from '../../src/contexts/BirthdayProvider';
 test('addBirthday add a new birthday in the list', () => {
-  const {result} = renderHook(() => useCounter(), {wrapper: BirthdayProvider});
-  expect(result.current.birthdays).toEqual([]);
+  const {result: hook} = renderHook(() => useBirthdayStore(), {
+    wrapper: BirthdayProvider,
+  });
+  expect(hook.current.birthdays).toEqual([]);
 
   act(() => {
-    result.current.addBirthday({
+    hook.current.addBirthday({
       date: new Date(2020, 3, 29, 0, 0, 0),
       name: 'Ro',
     });
   });
 
-  expect(result.current.birthdays[0].name).toEqual('Ro');
-  expect(result.current.birthdays[0].date).toEqual(
+  expect(hook.current.birthdays[0].name).toEqual('Ro');
+  expect(hook.current.birthdays[0].date).toEqual(
     new Date(2020, 3, 29, 0, 0, 0),
   );
-  expect(result.current.birthdays[0].id).not.toBe(undefined);
+  expect(hook.current.birthdays[0].id).not.toBe(undefined);
+});
+
+test('nextBirthday returns the next happening birthday', () => {
+  const {result: hook} = renderHook(() => useBirthdayStore(), {
+    wrapper: BirthdayProvider,
+  });
+  expect(hook.current.birthdays).toEqual([]);
+
+  act(() => {
+    hook.current.addBirthday({
+      date: subYears(addMonths(Date.now(), 1), 80),
+      name: 'Grandma',
+    });
+  });
+
+  act(() => {
+    hook.current.addBirthday({
+      date: subYears(addMonths(Date.now(), -1), 30),
+      name: 'Me',
+    });
+  });
+
+  act(() => {
+    hook.current.addBirthday({
+      date: subYears(addMonths(Date.now(), 2), 60),
+      name: 'Dad',
+    });
+  });
+
+  expect(hook.current.nextBirthday().name).toEqual('Grandma');
 });
--- src/hooks/useBirthdayStore.ts
+++ src/hooks/useBirthdayStore.ts
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react';
 import uuid from 'uuid-random';
 import BirthdaysContext, {Birthday} from '../contexts/BirthdaysContext';
+import {setYear, getYear, differenceInDays} from 'date-fns';
 
 const useBirthdayStore = () => {
   const [birthdays, setBirthdays] = useContext(BirthdaysContext);
@@ -12,7 +13,21 @@ const useBirthdayStore = () => {
     setBirthdays([...birthdays, birthday]);
   };
 
-  return {birthdays, addBirthday};
+  const nextBirthday = () => {
+    const thisYear = getYear(Date.now());
+    const sortedBirthdays = birthdays.sort((bdate1, bdate2) =>
+      differenceInDays(
+        setYear(bdate1.date, thisYear),
+        setYear(bdate2.date, thisYear),
+      ),
+    );
+    return sortedBirthdays.find(
+      (bdate) =>
+        differenceInDays(setYear(bdate.date, thisYear), Date.now()) >= 0,
+    );
+  };
+
+  return {birthdays, addBirthday, nextBirthday};
 };
 
 export default useBirthdayStore;
